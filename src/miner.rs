c31081a882d6f5a8a1c584a8cd8f7e86a3eb6293
Martin <martin@localhost.localdomain>
2023-10-22 17:18:30 -0300

    Deployed formatter to show model progress. Moved template parameters into definition of struct

--- src/miner.rs
+++ src/miner.rs
@@ -1,71 +1,72 @@
 
-use std::fmt::Debug;
-
 use crate::*;
+use crate::io::PrettyFormatter;
+use crate::model::BernoulliAssignment;
 
 
-pub trait Miner {
-    fn mine<'a, D, M>( &'a mut self, data: &'a D, model: &'a mut M ) where
-	D: Database + 'a,
-    &'a D: IntoIterator<Item = DataPair>,
-	M: Model + Loggable + 'a,
-	M::Candidate: Loggable;
+pub trait Miner<'a, D, M> {
+    fn mine( &'a mut self, data: &'a D, model: &'a mut M );
 }
 
-pub struct EmMiner {
+pub struct EmMiner<D, M> {
     max_iterations: u64,
+    data: Option<D>,
+    model: Option<M>,
+    model_formatter: Option<Box<dyn PrettyFormatter<M>>>,
 }
 
-impl Miner for EmMiner {
-    fn mine<'a, D, M>( &'a mut self, data: &'a D, model: &'a mut M ) where
-	D: Database + 'a,
-    &'a D: IntoIterator<Item = DataPair>,
-	M: Model + Loggable + 'a,
-	M::Candidate: Loggable,
-    {
+impl <'a, D: Database, M: Model> Miner<'a, D, M> for EmMiner<D,M> where
+    &'a D: IntoIterator<Item = DataPair> + 'a,
+{
+    fn mine( &'a mut self, data: &'a D, model: &'a mut M ) where {
 	let mut last_loglik = f64::NEG_INFINITY;
 	let mut loglik = self.initialize( model, data );
 	let mut iteration = 0;
 	while last_loglik < loglik {
 	    iteration += 1;
-	    let iteration_span = info_span!( "iteration", number = iteration );
-	    let last_loglik = loglik;
+	    let _iteration_span = info_span!( "iteration", number = iteration );
+	    last_loglik = loglik;
 	    loglik = self.iterate( model, data, loglik );
 
 	    info!( "Likelihood changed from {last_loglik:.3} to {loglik:.3}" );
+	    if let Some(formatter) = &self.model_formatter {
+		debug!( "{}", formatter.format_pretty( model ));
+	    }
 	}
     }
 }
 
 
 
-impl EmMiner {
-    pub fn new( max_iterations: u64 ) -> EmMiner {
+impl <'a, D: Database, M: Model> EmMiner<D, M> where
+    &'a D: IntoIterator<Item = DataPair> + 'a,
+{
+    pub fn new( max_iterations: u64 ) -> EmMiner<D, M> {
 	EmMiner {
 	    max_iterations,
+	    data: None,
+	    model: None,
+	    model_formatter: None,
 	}
     }
 
-    fn initialize<'a, M: Model, D: Database>( &self, model: &mut M, data: &'a D ) -> f64 where
-	D: Database + 'a,
-    &'a D: IntoIterator<Item = DataPair>,
-	M: Model + Loggable + 'a,
-    {
-	let init_span = info_span!( "initialization" );
-	let mut loglik = self.step( model, data );
+    pub fn provide_model_formatter<F: PrettyFormatter<M> + 'static>( &mut self, formatter: F ) {
+	self.model_formatter = Some( Box::new( formatter ));
+    }
 
-	model.log( "initial model", Level::DEBUG );
+    fn initialize( &self, model: &mut M, data: &'a D ) -> f64 {
+	let _init_span = info_span!( "initialization" );
+	let loglik = self.step( model, data );
+
 	info!( "initial score {}", loglik );
+	// if let Some(formatter) = self.model_formatter {
+	    // debug!( "{}", formatter.format_pretty( model ));
+	// }
 
 	loglik
     }
 
-    fn iterate<'a, M: Model, D: Database>( &self, model: &mut M, data: &'a D, loglik: f64 ) -> f64 where
-	D: Database + 'a,
-    &'a D: IntoIterator<Item = DataPair>,
-	M: Model + Loggable + 'a,
-    M::Candidate: Loggable,
-    {
+    fn iterate( &self, model: &mut M, data: &'a D, loglik: f64 ) -> f64 {
 	let success = self.grow( model, data, loglik );
 	if success {
 	    debug!( "Candidate accepted" );
@@ -76,17 +77,14 @@ impl EmMiner {
 	}
     }
 
-    fn step<'a, M: Model, D: Database>( &self, model: &mut M, data: &'a D ) -> f64
-    where
-	&'a D: IntoIterator<Item = DataPair>, D: 'a
+    fn step( &self, model: &mut M, data: &'a D ) -> f64
     {
 	let cover = model.cover( data.into_iter() );
 	model.fit( &cover );
 	model.calc_loglik( &cover )
     }
 
-    fn optimize<'a, M: Model, D: Database>( &self, model: &mut M, data: &'a D, base_loglik: f64 ) -> f64 where
-	&'a D: IntoIterator<Item = DataPair>,
+    fn optimize( &self, model: &mut M, data: &'a D, base_loglik: f64 ) -> f64 where
     {
 	let mut loglik = base_loglik;
 	let mut last_loglik = f64::NEG_INFINITY;
@@ -98,13 +96,10 @@ impl EmMiner {
 	loglik
     }
 
-    fn grow<'a, M: Model, D: Database + 'a>( &self, model: &mut M, data: &'a D, loglik: f64 ) -> bool where
-	&'a D: IntoIterator<Item = DataPair>,
-	M::Candidate: Loggable,
-    {
+    fn grow( &self, model: &mut M, data: &'a D, loglik: f64 ) -> bool {
 	let candidates = model.generate_candidates( data );
 	for mut current in candidates {
-	    current.log( "adding candidate", Level::DEBUG );
+	    // current.log( "adding candidate", Level::DEBUG );
 	    
 	    model.add_candidate( &mut current );
 	    let next_loglik = self.step( model, data );
